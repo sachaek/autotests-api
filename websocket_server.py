31948bee603b3a4b08f48c3fc0d3752f7b3a5576
sachaek <sacha-ek@yandex.ru>
2025-10-05 13:17:39 +0500

    websocket server add

---
+++ websocket_server.py
@@ -0,0 +1,19 @@
+import asyncio
+
+import websockets
+from websockets import ServerConnection
+
+
+async def echo(websocket: ServerConnection):
+    async for message in websocket:
+        print(f"Получено сообщение: {message}")
+        response = f"Сервер получил: {message}"
+        await websocket.send(response)
+
+
+async def main():
+    server = await websockets.serve(echo, host="localhost", port=8756)
+    print("Websocket сервер запущен на ws://localhost:8756")
+    await server.wait_closed()
+
+asyncio.run(main())
