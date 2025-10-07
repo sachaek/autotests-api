6446b28363861b68b760837d11497265112293ca
sachaek <sacha-ek@yandex.ru>
2025-10-07 14:15:24 +0500

    httpx continue

---
+++ httpx_example.py
@@ -0,0 +1,37 @@
+import httpx
+
+response = httpx.get("https://jsonplaceholder.typicode.com/todos/1")
+print(response.status_code)
+print(response.json())
+
+data = {
+    "title": "Новая задача",
+    "completed": False,
+    "userId": 1
+}
+
+response = httpx.post("https://jsonplaceholder.typicode.com/todos", json=data)
+
+print(response.status_code)
+print(response.request.headers)
+print(response.json())
+
+data = {"username": "test_user", "password": "123456"}
+
+# response = httpx.post("https://httpbin.org/post", data=data)
+# print(response)  # {'form': {'username': 'test_user', 'password': '123456'}, ...}
+
+# headers = {"Authorization": "Bearer my_secret_token"}
+# response = httpx.get("https://httpbin.org/get", headers=headers)
+# print(response.request.headers)
+# print(response.json())
+
+params = {"userId": 1}
+response = httpx.get("https://jsonplaceholder.typicode.com/todos", params=params)
+print(response.url)
+print(response.json())
+
+files = {"file": ("example.txt", open("example.txt", "rb"))}
+response = httpx.post("https://httpbin.org/post", files=files)
+
+print(response.json())
