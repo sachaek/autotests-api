c57475ae06225a67b6bb797202dd0203eda1da2b
sachaek <sacha-ek@yandex.ru>
2025-10-10 21:50:25 +0500

    автотесты с неработающими ручками

--- httpx_example.py
+++ httpx_example.py
@@ -1,23 +1,23 @@
 import httpx
 
-response = httpx.get("https://jsonplaceholder.typicode.com/todos/1")
-print(response.status_code)
-print(response.json())
-
-data = {
-    "title": "Новая задача",
-    "completed": False,
-    "userId": 1
-}
-
-response = httpx.post("https://jsonplaceholder.typicode.com/todos", json=data)
-
-print(response.status_code)
-print(response.request.headers)
-print(response.json())
-
-data = {"username": "test_user", "password": "123456"}
-
+# response = httpx.get("https://jsonplaceholder.typicode.com/todos/1")
+# print(response.status_code)
+# print(response.json())
+#
+# data = {
+#     "title": "Новая задача",
+#     "completed": False,
+#     "userId": 1
+# }
+#
+# response = httpx.post("https://jsonplaceholder.typicode.com/todos", json=data)
+#
+# print(response.status_code)
+# print(response.request.headers)
+# print(response.json())
+#
+# data = {"username": "test_user", "password": "123456"}
+#
 # response = httpx.post("https://httpbin.org/post", data=data)
 # print(response)  # {'form': {'username': 'test_user', 'password': '123456'}, ...}
 
@@ -25,13 +25,23 @@ data = {"username": "test_user", "password": "123456"}
 # response = httpx.get("https://httpbin.org/get", headers=headers)
 # print(response.request.headers)
 # print(response.json())
+#
+# params = {"userId": 1}
+# response = httpx.get("https://jsonplaceholder.typicode.com/todos", params=params)
+# print(response.url)
+# print(response.json())
+#
+# files = {"file": ("example.txt", open("example.txt", "rb"))}
+# response = httpx.post("https://httpbin.org/post", files=files)
+#
+# print(response.json())
 
-params = {"userId": 1}
-response = httpx.get("https://jsonplaceholder.typicode.com/todos", params=params)
-print(response.url)
-print(response.json())
 
-files = {"file": ("example.txt", open("example.txt", "rb"))}
-response = httpx.post("https://httpbin.org/post", files=files)
+with httpx.Client() as client:
+    response1 = client.get("https://jsonplaceholder.typicode.com/todos/1")
+    response2 = client.get("https://jsonplaceholder.typicode.com/todos/2")
 
-print(response.json())
+print(response1.json())
+print(response2.json())
+
+client = httpx.Client(headers={"Authorization": "Bearer my_secret_token"})
