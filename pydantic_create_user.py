26479f2fb31a7d4a00661d7de63c2393ccaa9a2c
Alexander <sacha-ek@yandex.ru>
2026-05-08 11:33:46 +0500

    поправил модель

--- pydantic_create_user.py
+++ pydantic_create_user.py
@@ -1,7 +1,10 @@
-from pydantic import BaseModel, EmailStr, Field
+from pydantic import BaseModel, EmailStr, Field, ConfigDict
+
+from tools.fakers import fake
 
 
 class UserSchema(BaseModel):
+    model_config = ConfigDict(populate_by_name=True)
     id: str
     email: EmailStr
     last_name: str = Field(alias="lastName")
@@ -10,11 +13,15 @@ class UserSchema(BaseModel):
 
 
 class CreateUserRequestSchema(BaseModel):
-    email: EmailStr
-    password: str
-    last_name: str = Field(alias="lastName")
-    first_name: str = Field(alias="firstName")
-    middle_name: str = Field(alias="middleName")
+    """
+    Описание структуры запроса на создание пользователя.
+    """
+    model_config = ConfigDict(populate_by_name=True)
+    email: EmailStr = Field(default_factory=fake.email)
+    password: str = Field(default_factory=fake.password())
+    last_name: str = Field(alias="lastName", default_factory=fake.last_name())
+    first_name: str = Field(alias="firstName", default_factory=fake.first_name())
+    middle_name: str = Field(alias="middleName", default_factory=fake.middle_name())
 
 
 class CreateUserResponseSchema(BaseModel):
