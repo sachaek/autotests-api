c2b303b6f2dd1fa23c3819719cc4022cbba2d6d2
Alexander <sacha-ek@yandex.ru>
2026-05-08 11:37:31 +0500

    поправили user_schema

--- pydantic_create_user.py
+++ pydantic_create_user.py
@@ -17,11 +17,17 @@ class CreateUserRequestSchema(BaseModel):
     Описание структуры запроса на создание пользователя.
     """
     model_config = ConfigDict(populate_by_name=True)
+
+    # Добавили генерацию случайного email
     email: EmailStr = Field(default_factory=fake.email)
-    password: str = Field(default_factory=fake.password())
-    last_name: str = Field(alias="lastName", default_factory=fake.last_name())
-    first_name: str = Field(alias="firstName", default_factory=fake.first_name())
-    middle_name: str = Field(alias="middleName", default_factory=fake.middle_name())
+    # Добавили генерацию случайного пароля
+    password: str = Field(default_factory=fake.password)
+    # Добавили генерацию случайной фамилии
+    last_name: str = Field(alias="lastName", default_factory=fake.last_name)
+    # Добавили генерацию случайного имени
+    first_name: str = Field(alias="firstName", default_factory=fake.first_name)
+    # Добавили генерацию случайного отчества
+    middle_name: str = Field(alias="middleName", default_factory=fake.middle_name)
 
 
 class CreateUserResponseSchema(BaseModel):
