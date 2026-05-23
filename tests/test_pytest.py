e031a8b5764e7e7e59d80d26f727ecfd0c63df46
Alexander <sacha-ek@yandex.ru>
2026-05-23 21:21:12 +0500

    update pytest

---
+++ tests/test_pytest.py
@@ -0,0 +1,10 @@
+def test_first_try():
+    print("Hello World!")
+
+
+def test_assert_positive_case():
+    assert (2 + 2) == 4  # Ожидается, что тест пройдет
+
+
+def test_assert_negative_case():
+    assert (2 + 2) == 5  # Тут должна быть ошибка
