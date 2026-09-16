eb24f55440b021c82806b8c682f2e321af4b6be9
Anton <81878327+anton-kazantsev@users.noreply.github.com>
2026-09-16 15:27:37 +0500

    8.5 Фикстуры Pytest

--- tests/test_authentication.py
+++ tests/test_authentication.py
@@ -2,10 +2,11 @@ from http import HTTPStatus
 
 import pytest
 
-from clients.authentication.authentication_client import get_authentication_client
+from clients.authentication.authentication_client import AuthenticationClient
 from clients.authentication.authentication_schema import LoginRequestSchema, LoginResponseSchema
-from clients.users.public_users_client import get_public_users_client
+from clients.users.public_users_client import PublicUsersClient
 from clients.users.users_schema import CreateUserRequestSchema
+from tests.conftest import UserFixture
 from tools.assertions.authentication import assert_login_response
 from tools.assertions.base import assert_status_code
 from tools.assertions.schema import validate_json_schema
@@ -13,32 +14,23 @@ from tools.assertions.schema import validate_json_schema
 
 @pytest.mark.authentication  # Добавили маркировку authentication
 @pytest.mark.regression  # Добавили маркировку regression
-def test_login():
-    # Инициализируем API-клиент для работы с пользователями
-    public_users_client = get_public_users_client()
-    authentication_client = get_authentication_client()
-
-    # Формируем тело запроса на создание пользователя
-    create_user_request = CreateUserRequestSchema()
-    # Отправляем запрос на создание пользователя
-    public_users_client.create_user(create_user_request)
-
+def test_login(function_user: UserFixture, public_users_client: PublicUsersClient, authentication_client: AuthenticationClient):
     # Формируем тело запроса на авторизацию с использованием данных созданного пользователя
-    login_request = LoginRequestSchema(
-        email=create_user_request.email,
-        password=create_user_request.password
+    request = LoginRequestSchema(
+        email=function_user.email,
+        password=function_user.password
     )
     # Отправляем запрос на авторизацию и получаем ответ
-    login_response = authentication_client.login_api(login_request)
+    response = authentication_client.login_api(request)
     # Инициализируем модель ответа на основе полученного JSON в ответе
     # Также благодаря встроенной валидации в Pydantic дополнительно убеждаемся, что ответ корректный
-    login_response_data = LoginResponseSchema.model_validate_json(login_response.text)
+    response_data = LoginResponseSchema.model_validate_json(response.text)
 
     # Используем функцию для проверки статус-кода
-    assert_status_code(login_response.status_code, HTTPStatus.OK)
+    assert_status_code(response.status_code, HTTPStatus.OK)
     
     # Используем функцию для проверки ответа создания юзера
-    assert_login_response(login_response_data)
+    assert_login_response(response_data)
 
     # Проверяем, что тело ответа соответствует ожидаемой JSON-схеме
-    validate_json_schema(login_response.json(), login_response_data.model_json_schema())
+    validate_json_schema(response.json(), response_data.model_json_schema())
--- tests/test_users.py
+++ tests/test_users.py
@@ -2,7 +2,7 @@ from http import HTTPStatus
 
 import pytest
 
-from clients.users.public_users_client import get_public_users_client
+from clients.users.public_users_client import PublicUsersClient
 from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema
 from tools.assertions.base import assert_status_code
 from tools.assertions.schema import validate_json_schema
@@ -11,14 +11,11 @@ from tools.assertions.users import assert_create_user_response
 
 @pytest.mark.users  # Добавили маркировку users
 @pytest.mark.regression  # Добавили маркировку regression
-def test_create_user():
-    # Инициализируем API-клиент для работы с пользователями
-    publick_user_client = get_public_users_client()
-
+def test_create_user(public_users_client: PublicUsersClient): # Используем фикстуру API клиента
     # Формируем тело запроса на создание пользователя
     request = CreateUserRequestSchema()
    # Отправляем запрос на создание пользователя
-    response = publick_user_client.create_user_api(request)
+    response = public_users_client.create_user_api(request)
     # Инициализируем модель ответа на основе полученного JSON в ответе
     # Также благодаря встроенной валидации в Pydantic дополнительно убеждаемся, что ответ корректный
     response_data = CreateUserResponseSchema.model_validate_json(response.text)
