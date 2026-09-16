7080816f30d0147d4e84c15b07981dc7346d848e
Anton <81878327+anton-kazantsev@users.noreply.github.com>
2026-09-16 16:59:52 +0500

    Написал тест на получение текущего пользователя

--- tests/test_users.py
+++ tests/test_users.py
@@ -2,11 +2,13 @@ from http import HTTPStatus
 
 import pytest
 
+from clients.users.private_users_client import PrivateUsersClient
 from clients.users.public_users_client import PublicUsersClient
-from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema
+from clients.users.users_schema import CreateUserRequestSchema, CreateUserResponseSchema, GetUserResponseSchema
+from tests.conftest import UserFixture
 from tools.assertions.base import assert_status_code
 from tools.assertions.schema import validate_json_schema
-from tools.assertions.users import assert_create_user_response
+from tools.assertions.users import assert_create_user_response, assert_get_user_response
 
 
 @pytest.mark.users  # Добавили маркировку users
@@ -25,6 +27,22 @@ def test_create_user(public_users_client: PublicUsersClient): # Использу
 
     # Используем функцию для проверки ответа создания юзера
     assert_create_user_response(request, response_data)
+
+    # Проверяем, что тело ответа соответствует ожидаемой JSON-схеме
+    validate_json_schema(response.json(), response_data.model_json_schema())
+
+
+@pytest.mark.users
+@pytest.mark.regression
+def test_get_user_me(private_users_client: PrivateUsersClient, function_user: UserFixture):
+    response = private_users_client.get_user_me_api()
+    response_data = GetUserResponseSchema.model_validate_json(response.text)
+
+    # Используем функцию для проверки статус-кода
+    assert_status_code(response.status_code, HTTPStatus.OK)
+
+    # Проверка созданного пользователя
+    assert_get_user_response(function_user.response, response_data)
 
     # Проверяем, что тело ответа соответствует ожидаемой JSON-схеме
     validate_json_schema(response.json(), response_data.model_json_schema())
