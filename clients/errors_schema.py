c715495347788629ce4ea24d0fabbf9a73767999
Anton <81878327+anton-kazantsev@users.noreply.github.com>
2026-09-19 21:40:04 +0500

    9.2 Практикуемся в написании API автотестов. Часть 1

---
+++ clients/errors_schema.py
@@ -0,0 +1,30 @@
+from pydantic import BaseModel, Field, ConfigDict
+from typing import Any
+
+
+class ValidationErrorsSchema(BaseModel):
+    """
+    Модель, описывающая структуру ошибки валидации API.
+    """
+    model_config = ConfigDict(populate_by_name=True)
+
+    type: str
+    input: Any
+    context: dict[str, Any] = Field(alias="ctx")
+    message: str = Field(alias="msg")
+    location: list[str] = Field(alias="loc")
+
+
+class ValidationErrorResponseSchema(BaseModel):
+    """
+    Модель, описывающая структуру ответа API с ошибкой валидации.
+    """
+    model_config = ConfigDict(populate_by_name=True)
+
+    details: list[ValidationErrorsSchema] = Field(alias="detail")
+
+
+class InternalErrorResponseSchema(BaseModel):
+    model_config = ConfigDict(populate_by_name=True)
+
+    details: str = Field(alias="detail")
--- clients/files/files_schema.py
+++ clients/files/files_schema.py
@@ -29,3 +29,10 @@ class CreateFileResponseSchema(BaseModel):
     Описание структуры ответа создания файла.
     """
     file: FileSchema
+
+
+class GetFileResponseSchema(BaseModel):
+    """
+    Описание структуры ответа получения файла.
+    """
+    file: FileSchema
