97fa71d0cd09f23d99a6ddc7607745fca80b60ff
Anton <81878327+anton-kazantsev@users.noreply.github.com>
2026-09-21 17:45:10 +0500

    добавил тест на создание курса

--- tests/courses/test_courses.py
+++ tests/courses/test_courses.py
@@ -3,11 +3,12 @@ from http import HTTPStatus
 import pytest
 
 from clients.courses.courses_client import CoursesClient
-from clients.courses.courses_schema import GetCoursesQuerySchema, GetCoursesResponseSchema, UpdateCourseRequestSchema, UpdateCourseResponseSchema
+from clients.courses.courses_schema import CreateCourseRequestSchema, CreateCourseResponseSchema, GetCoursesQuerySchema, GetCoursesResponseSchema, UpdateCourseRequestSchema, UpdateCourseResponseSchema
 from fixtures.courses import CourseFixture
+from fixtures.files import FileFixture
 from fixtures.users import UserFixture
 from tools.assertions.base import assert_status_code
-from tools.assertions.courses import assert_get_courses_response, assert_update_course_response
+from tools.assertions.courses import assert_create_course_response, assert_get_courses_response, assert_update_course_response
 from tools.assertions.schema import validate_json_schema
 
 
@@ -36,5 +37,23 @@ class TestCourses:
 
         assert_status_code(response.status_code, HTTPStatus.OK)
         assert_update_course_response(request, response_data)
+
+        validate_json_schema(response.json(), response_data.model_json_schema())
+
+    def test_create_course(
+            self, 
+            courses_client: CoursesClient,
+            function_file: FileFixture,
+            function_user: UserFixture
+            ):
+        request = CreateCourseRequestSchema(
+            preview_file_id = function_file.response.file.id,
+            created_by_user_id = function_user.response.user.id
+        )
+        response = courses_client.create_course_api(request)
+        response_data = CreateCourseResponseSchema.model_validate_json(response.text)
+
+        assert_status_code(response.status_code, HTTPStatus.OK)
+        assert_create_course_response(request, response_data)
 
         validate_json_schema(response.json(), response_data.model_json_schema())
